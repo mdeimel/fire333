b416e9d8f20ee867197a4d9a26e222e8c9fae96f
mdeimel <mdeimel@gmail.com>
2011-04-15 22:23:33 -0500

    new app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fire333::Application.config.secret_token = 'd7a2009c042429ecddd663f6e86e9519d4675ce1e593e7efa125f2990e035cd40d76c79a8fddddbdc2e76e011b2f0b6f5b89ec71eb033d3e1ec8ad5bcdd7acd6'
